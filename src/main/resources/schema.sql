e783951651174afbf3e236595aa1db185f5da3a4
kimjonghyo <qaws2921@hanmail.net>
2022-07-27 11:19:04 +0900

    first commit

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-    user_id bigint not null primary key auto_increment,
-    username varchar(50) not null unique,
-    password varchar(100),
-    role varchar(100) default 'ROLE_USER'
-)
